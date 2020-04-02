8bda9b1d469de30472fc103bf9b73d7c6a3d6d80
Jefferson Kalffman <jefferson@kalffman.com>
2020-04-02 11:38:26 -0400

    Atualizando dependencias de adSense

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,4 +1,4 @@
-import React,{ Component} from 'react';
+import React,{ Component } from 'react';
 import './Calculator.css';
 import Button from '../components/Button';
 import Display from '../components/Display';
